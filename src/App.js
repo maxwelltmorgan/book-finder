3b5e1af00b5ffcde43ebe787aa1a6876aa3ad269
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-03-03 00:32:50 -0700

    log api error

--- src/App.js
+++ src/App.js
@@ -7,28 +7,32 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state={
+    this.state = {
       items: []
     };
-  };
+  }
+
+  handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+  }
 
   search = (e) => {
     e.preventDefault();
     const topic = e.target.elements.topic.value;
     const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=" + topic;
     fetch(BASE_URL, {method:"GET"})
+    .then(this.handleErrors)
     .then(response =>  response.json())
     .then(json => {
       let {items} = json;
       this.setState({
         items: items
       })
-      console.log(json);
-      console.log(items[0].volumeInfo.imageLinks.thumbnail);
-      console.log(items[0].volumeInfo.title);
-      console.log(items[0].volumeInfo.authors[0]);
-      console.log(items[0].volumeInfo.publisher);
-      console.log(items[0].volumeInfo.infoLink);
+    }).catch(function() {
+      console.log("error");
     })
   };
 
